927032dc2d3c7c7795712d26eda40ddda793ce3b
Daisuke <Yamagutino@greenApple.local>
2016-11-15 19:35:08 +0900

    added expression parser

--- src/NezType.hs
+++ src/NezType.hs
@@ -1,79 +1,116 @@
 module NezType where
 
-import Control.Arrow
-import Text.ParserCombinators.Parsec
+import Control.Applicative ((<*))
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Expr
+import Text.Parsec.Token
+import Text.Parsec.Language
 
-type Tag = String
+{-Nez Type definition-}
+type NEZType = (String, Ty)
 
-data Token = Epsilon
-           | TString String
-           | Any
-           deriving (Eq,Show)
+data Ty = Ttoken String String -- (tagName, expression)
+        | Tlist String Ty
+        | Ttuple String [Ty]
+        | Ttree String [(String,Ty)] -- (tagName, [(label, Ty)])
+        | Toption Ty
+        | Tunion [Ty]
+        | Trecursive Ty
+        | Tname String
+        deriving (Eq, Show)
 
-type LabeledTree = (String, Value)
+{-parser definition-}
+languageDefinition = javaStyle{ reservedOpNames = ["<", ">", "=", "/", "$", "-", "&", "!", "{$"]
+                              , reservedNames = ["public", "inline", "import", "from", "grammar", "example", "template", "format", "true", "false", "define"]
+                            }
 
-getLabel :: LabeledTree -> String
-getLabel = fst
+tokenParser = makeTokenParser languageDefinition
 
-data Value = VToken Tag Token -- VToken {ty :: Tag, value :: Token}
-           | VTuple Tag [Value] -- VTuple {ty :: Tag, value :: [Value]}
-           | VChoice Tag [Value] -- VChoice {ty :: Tag, value :: [Value]}
-           | VOption Tag Value -- VOption {ty :: Tag, value :: Value}
-           | VList Tag Value -- VList {ty :: Tag, value :: Value}
-           | VTree Tag [LabeledTree] -- | VTree {ty :: Tag, value :: [LabeledTree]}
-           deriving (Eq,Show)
+nezParens = parens tokenParser
+nezBraces = braces tokenParser
+nezBrackets = brackets tokenParser
+nezSquare = squares tokenParser
+nezSemiColon = semi tokenParser
+nezComma = comma tokenParser
+nezDot = dot tokenParser
+nezSemiSep1 = semiSep1 tokenParser
+nezCommaSep1 = commaSep1 tokenParser
+nezIdentifier = identifier tokenParser
+nezReservedOp = reservedOp tokenParser
+nezReserved = reserved tokenParser
+nezWhiteSpace = whiteSpace tokenParser
 
-type TypedSubNode = (String, Ty)
+stmt :: Parser NEZType
+stmt =  try exampleStatement
+    <|> try importStatement
+    <|> try formatStatement
+    <|> try templateStatement
+    <|> production
+    <?> "statement"
 
-data Ty = TToken Tag Token
-        | TTuple Tag [Ty]
-        | TChoice Tag [Ty]
-        | TOption Tag Ty
-        | TList Tag Ty
-        | TTree Tag [TypedSubNode]
-        deriving (Eq,Show)
+importStatement :: Parser NEZType
+importStatement = do
+  nezReserved "import"
+  name <- nezCommaSep1 nezIdentifier
+  nezReserved "from"
+  nezIdentifier
+  return ("import", Tname (concat name))--fixme or remove me
 
-typingFunc :: Value -> Ty
-typingFunc (VToken tag token) = TToken tag token
-typingFunc (VTuple tag vs) = TTuple tag (fmap typingFunc vs)
-typingFunc (VChoice tag vs) = TChoice tag (fmap typingFunc vs)
-typingFunc (VOption tag v) = TOption tag (typingFunc v)
-typingFunc (VList tag v) = TList tag (typingFunc v)
-typingFunc (VTree tag lts) = TTree tag (fmap (second typingFunc) lts)
+production :: Parser NezType
+production = do
+  many (nezReserved "public" <|> nezReserved "inline")
+  name <- nezIdentifier
+  nezReservedOp "="
+  nezExpression <- expression
+  return ('T':name, nezExpression)
 
--- Parser ----------------------------
-nezFile = endBy line eol
+expression :: Parser Ty
+expression = buildExpressionParser table term <?> "expression"
+  where
+    table = [[Suffix (nezReservedOp "*" >> return . typingFunc2List)]
+           , [Suffix (nezReservedOp "+" >> return . typingFunc2ZeroMore)]
+           , [Suffix (nezReservedOp "?" >> return . typingFunc2Optional)]
+           , [Prefix (nezReservedOp "&" >> return)]
+           , [Prefix (nezReservedOp "!" >> return)]
+           , [Prefix (nezReservedOp "$" >> return)] -- fixme
+           ]
 
-line =  try (productionRule)
-    <|> try comment
-    <?> "Lines"
+typingFunc2List :: Ty -> Ty
+typingFunc2List (Ttoken tag peg) = Tlist tag (Ttoken tag peg)
+typingFunc2List (Tlist tag ty) = Tlist tag ty
+typingFunc2List (Ttuple tag tys) = Tlist tag (Ttuple tag tys)
+typingFunc2List (Ttree tag sub) = Tlist tag (Ttree tag sub)
+typingFunc2List (Toption ty) = Tlist [] (Toption ty)
+typingFunc2List (Tunion tys) = Tlist [] (Tunion tys)
+typingFunc2List (Trecursive ty) = Tlist [] (Trecursive ty)
+typingFunc2List (Tname name) = Tlist [] (Tname name)
 
-comment = (string "/*" >> manyTill anyChar ((try (string "*/") >> return Nothing <|> eof) >> spaces >> return Nothing
+typingFunc2ZeroMore :: Ty  -> Ty
+typingFunc2ZeroMore (Ttoken tag peg) = Ttuple tag [Ttoken tag peg, Tlist tag (Ttoken tag peg)]
+typingFunc2ZeroMore (Tlist tag ty) = Tlist tag ty
+typingFunc2ZeroMore (Ttuple tag tys) = Ttuple tag [Ttuple tag tys, Tlist tag (Ttuple tag tys)]
+typingFunc2ZeroMore (Ttree tag sub) = Ttuple tag [Ttree tag sub, Tlist tag (Ttree tag sub)]
+typingFunc2ZeroMore (Toption ty) = Tlist [] ty
+typingFunc2ZeroMore (Tunion tys) = Ttuple [] [Tunion tys, Tlist [] (Tunion tys)]
+typingFunc2ZeroMore (Trecursive ty) = Ttuple [] [Trecursive ty, Tlist [] (Trecursive ty)]
+typingFunc2ZeroMore (Tname name) = Ttuple [] [Tname name, Tlist [] (Tname name)]
 
-productionRule = do
-  ruleID <- many (noneOf " ,=\n")
-  spaces
-  char '='
-  spaces
-  expr <- nezExpression
-  return Just $ (ruleID, expr)
+typingFunc2Optional :: Ty -> Ty
+typingFunc2Optional (Toption ty) = Toption ty
+typingFunc2Optional ty = Toption ty
 
-nezExpression =  try (tagedTree)
-             <|> try (tagedTuple)
-             <|> try (tagedChoice)
-             <?> "nez expression"
 
-tagedTree =  try (leftFolding)
-         <|> try (normalTree)
-         <?> "taged Tree"
 
-normalTree = do
-  char '{'
-  manyTill anyChar (char '$')
-  labelid <- manyTill anyChar (char '(')
-  char '#'
-  tag <- manyTill anyChar (space <|> char '}')
-  return $ VTree tag [(labelid, )]
+{-old expression
+nezExpression :: Parser Ty
+nezExpression =  do
+  choiceElements <- sepBy1 nezSequence (nezReservedOp "/")
+  return $ Tunion choiceElements
 
-eol =   string "\n\n"
-    <?> "end of line"
+nezSequence :: Parser Ty
+nezSequence = do
+  seqElements <- sepBy1 innar nezWhiteSpace
+
+  return expression
+-}
