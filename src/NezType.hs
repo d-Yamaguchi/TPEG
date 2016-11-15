fa237666deba0986284784072a83752808458e3a
Daisuke <Yamagutino@greenApple.local>
2016-11-15 22:01:51 +0900

    added infix operators and typingFunctions

--- src/NezType.hs
+++ src/NezType.hs
@@ -65,7 +65,7 @@ production = do
   nezExpression <- expression
   return ('T':name, nezExpression)
 
-expression :: Parser Ty
+expression :: Parser Ty -- sub tree operator $ is removed from table
 expression = buildExpressionParser table term <?> "expression"
   where
     table = [[Suffix (nezReservedOp "*" >> return . typingFunc2List)]
@@ -73,44 +73,37 @@ expression = buildExpressionParser table term <?> "expression"
            , [Suffix (nezReservedOp "?" >> return . typingFunc2Optional)]
            , [Prefix (nezReservedOp "&" >> return)]
            , [Prefix (nezReservedOp "!" >> return)]
-           , [Prefix (nezReservedOp "$" >> return)] -- fixme
+           , [Infix  (nezWhiteSpace >> return) AssocLeft]
+           , [Infix  (nezReservedOp "/" >> return) AssocLeft]
            ]
 
 typingFunc2List :: Ty -> Ty
-typingFunc2List (Ttoken tag peg) = Tlist tag (Ttoken tag peg)
-typingFunc2List (Tlist tag ty) = Tlist tag ty
-typingFunc2List (Ttuple tag tys) = Tlist tag (Ttuple tag tys)
-typingFunc2List (Ttree tag sub) = Tlist tag (Ttree tag sub)
-typingFunc2List (Toption ty) = Tlist [] (Toption ty)
-typingFunc2List (Tunion tys) = Tlist [] (Tunion tys)
-typingFunc2List (Trecursive ty) = Tlist [] (Trecursive ty)
-typingFunc2List (Tname name) = Tlist [] (Tname name)
+typingFunc2List s = case s of
+  (Ttoken tag peg) -> Tlist tag s
+  (Tlist tag ty) -> s
+  (Ttuple tag tys) -> Tlist tag s
+  (Ttree tag sub) -> Tlist tag s
+  (Toption ty) -> Tlist [] s
+  (Tunion tys) -> Tlist [] s
+  (Trecursive ty) -> Tlist [] s
+  (Tname name) -> Tlist [] s
 
 typingFunc2ZeroMore :: Ty  -> Ty
-typingFunc2ZeroMore (Ttoken tag peg) = Ttuple tag [Ttoken tag peg, Tlist tag (Ttoken tag peg)]
-typingFunc2ZeroMore (Tlist tag ty) = Tlist tag ty
-typingFunc2ZeroMore (Ttuple tag tys) = Ttuple tag [Ttuple tag tys, Tlist tag (Ttuple tag tys)]
-typingFunc2ZeroMore (Ttree tag sub) = Ttuple tag [Ttree tag sub, Tlist tag (Ttree tag sub)]
-typingFunc2ZeroMore (Toption ty) = Tlist [] ty
-typingFunc2ZeroMore (Tunion tys) = Ttuple [] [Tunion tys, Tlist [] (Tunion tys)]
-typingFunc2ZeroMore (Trecursive ty) = Ttuple [] [Trecursive ty, Tlist [] (Trecursive ty)]
-typingFunc2ZeroMore (Tname name) = Ttuple [] [Tname name, Tlist [] (Tname name)]
+typingFunc2ZeroMore s = case s of
+  (Ttoken tag peg) -> Ttuple tag [s, Tlist tag s]
+  (Tlist tag ty) -> s
+  (Ttuple tag tys) -> Ttuple tag [s, Tlist tag s]
+  (Ttree tag sub) -> Ttuple tag [s. Tlist tag s]
+  (Toption ty) -> Tlist [] ty
+  (Tunion tys) -> Ttuple [] [s, Tlist [] s]
+  (Trecursive ty) -> Ttuple [] [s, Tlist [] s]
+  (Tname name) -> Ttuple [] [s, Tlist [] s]
 
 typingFunc2Optional :: Ty -> Ty
-typingFunc2Optional (Toption ty) = Toption ty
-typingFunc2Optional ty = Toption ty
+typingFunc2Optional s = case s of
+  Toption ty -> s
+  _ -> Toption s
 
-
-
-{-old expression
-nezExpression :: Parser Ty
-nezExpression =  do
-  choiceElements <- sepBy1 nezSequence (nezReservedOp "/")
-  return $ Tunion choiceElements
-
-nezSequence :: Parser Ty
-nezSequence = do
-  seqElements <- sepBy1 innar nezWhiteSpace
-
-  return expression
--}
+typingFunc2Tuple :: Ty -> Ty -> Ty
+typingFunc2Tuple (Ttuple tag1 tys1) t2 =
+typingFunc2Tuple t1 (Ttuple tag2 tys2) = 
