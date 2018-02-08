268ff3313287a22f521c66352ea1f0e371ad053b
daisuke <systemf-omega@Daisukes-iMac.local>
2018-02-08 17:35:37 +0900

    refreshed

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # TPEG
-
-## exec
-* initialize stack
-```
-$ stack init
-```
-
-* build with Haskell stack.
-```
-$ stack build
-$ stack exec TPEG
-```
--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,6 @@
 module Main where
 
-import NezReader
-import Control.Monad
-import Text.Parsec
+import Lib
 
 main :: IO ()
-main = do
-  l <- getContents
-  runParseTest l
-
-runParseTest :: String -> IO()
-runParseTest xs  = case (parse start "" xs) of
-    Left err -> print err
-    Right str -> print "success"
+main = someFunc
